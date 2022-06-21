fed7ae51f7ea461dd240d2f64b4b11bd9912782d
hainh43 <hoanghai18497@gmail.com>
2022-06-21 11:09:16 +0700

    fix bug

--- gen_vector.py
+++ gen_vector.py
@@ -2,6 +2,6 @@ import gensim
 from gensim.test.utils import datapath, get_tmpfile
 from gensim.models import KeyedVectors
 from gensim.scripts.glove2word2vec import glove2word2vec
-glove_file = '../data/embeddings/glove.6B.300d.txt'
+glove_file = './data/embeddings/glove.6B.300d.txt'
 tmp_file = './data/embeddings/word2vec-glove.6B.300d.txt'
 glove2word2vec(glove_file, tmp_file)
