17956106dd9f0ed20fcdc55091741141640d4d9a
hainh43 <hoanghai18497@gmail.com>
2022-06-21 10:32:53 +0700

    add glove to vector

--- gen_vector.py
+++ gen_vector.py
@@ -1,4 +1,7 @@
 import gensim
 from gensim.test.utils import datapath, get_tmpfile
 from gensim.models import KeyedVectors
-# tmp_file = './data/embeddings/word2vec-glove.6B.300d.txt'
+from gensim.scripts.glove2word2vec import glove2word2vec
+glove_file = '../data/embeddings/glove.6B.300d.txt'
+tmp_file = './data/embeddings/word2vec-glove.6B.300d.txt'
+glove2word2vec(glove_file, tmp_file)
